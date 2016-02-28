85e6d36b334597566640a9bf334357418e954a3b
rafael-santiago <voidbrainvoid@gmail.com>
2016-02-27 21:20:20 -0300

    Some unit test for the "html" package

---
+++ src/tests/html_test.go
@@ -0,0 +1,41 @@
+/*
+ *                                Copyright (C) 2016 by Rafael Santiago
+ *
+ * This is a free software. You can redistribute it and/or modify under
+ * the terms of the GNU General Public License version 2.
+ *
+ */
+package cherry_test
+
+import (
+    "testing"
+    "../html"
+    "os"
+    "../config"
+    "../config/parser"
+)
+
+func PreprocessorBasicTest(t *testing.T) {
+    preprocessor := html.NewHtmlPreprocessor(nil)
+    if preprocessor.ExpandData("land-of-competition", "{{.FoD}} Zzz...") != "{{.FoD}} Zzz..." {
+        t.Fail()
+    }
+    var cherry_rooms *config.CherryRooms
+    cwd, _ := os.Getwd()
+    os.Chdir("../../sample")
+    var error *parser.CherryFileError
+    cherry_rooms, error = parser.ParseCherryFile("conf/sample.cherry")
+    os.Chdir(cwd)
+    if error != nil {
+       t.Fail()
+    }
+    preprocessor.Init(cherry_rooms)
+    preprocessor.SetDataValue("{{.foo}}", "bar")
+    preprocessor.SetDataValue("{{.bar}}", "foo")
+    if preprocessor.ExpandData("aliens-on-earth", "{{.foo}}{{.bar}}") != "barfoo" {
+        t.Fail()
+    }
+    if preprocessor.ExpandData("aliens-on-earth", "{{.greeting-message}}") != "Take meeeeee to your leader!!!" {
+        t.Fail()
+    }
+}
--- src/tests/rawhttp_test.go
+++ src/tests/rawhttp_test.go
@@ -1,11 +1,11 @@
 /*
- *                          Copyright (C) 2015, 2016 by Rafael Santiago
+ *                                Copyright (C) 2016 by Rafael Santiago
  *
  * This is a free software. You can redistribute it and/or modify under
  * the terms of the GNU General Public License version 2.
  *
  */
-package rawhttp
+package cherry_test
 
 import (
     "testing"

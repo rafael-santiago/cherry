1c251e27e5c6614dcac02d5ba2b79b3cba275225
rafael-santiago <voidbrainvoid@gmail.com>
2016-03-08 11:00:56 -0300

    Now the main package functions are private

--- src/cherry.go
+++ src/cherry.go
@@ -26,8 +26,7 @@ import (
 
 const cherryVersion = "1.1"
 
-// ProcessNewConnection handles the possible request with the specifc trap returned based on what is being requested.
-func ProcessNewConnection(newConn net.Conn, roomName string, rooms *config.CherryRooms) {
+func processNewConnection(newConn net.Conn, roomName string, rooms *config.CherryRooms) {
 	buf := make([]byte, 4096)
 	bufLen, err := newConn.Read(buf)
 	if err == nil {
@@ -41,8 +40,7 @@ func ProcessNewConnection(newConn net.Conn, roomName string, rooms *config.Cherr
 	}
 }
 
-// Peer is the room listener.
-func Peer(roomName string, c *config.CherryRooms) {
+func peer(roomName string, c *config.CherryRooms) {
 	port := c.GetListenPort(roomName)
 	var portNum int64
 	portNum, _ = strconv.ParseInt(port, 10, 16)
@@ -61,12 +59,11 @@ func Peer(roomName string, c *config.CherryRooms) {
 			fmt.Println(err.Error())
 			continue
 		}
-		go ProcessNewConnection(conn, roomName, c)
+		go processNewConnection(conn, roomName, c)
 	}
 }
 
-// GetOption handles the command line options.
-func GetOption(option, defaultValue string, flagOption ...bool) string {
+func getOption(option, defaultValue string, flagOption ...bool) string {
 	isFlagOption := false
 	if len(flagOption) > 0 {
 		isFlagOption = flagOption[0]
@@ -106,7 +103,7 @@ func openRooms(configPath string) {
 		rooms := cherryRooms.GetRooms()
 		for _, r := range rooms {
 			go messageplexer.RoomMessagePlexer(r, cherryRooms)
-			go Peer(r, cherryRooms)
+			go peer(r, cherryRooms)
 		}
 	}
 	sigintWatchdog := make(chan os.Signal, 1)
@@ -117,17 +114,17 @@ func openRooms(configPath string) {
 }
 
 func main() {
-	versionInfo := GetOption("version", "", true)
+	versionInfo := getOption("version", "", true)
 	if len(versionInfo) > 0 {
 		announceVersion()
 		os.Exit(0)
 	}
-	help := GetOption("help", "", true)
+	help := getOption("help", "", true)
 	if len(help) > 0 {
 		offerHelp()
 		os.Exit(0)
 	}
-	configPath := GetOption("config", "")
+	configPath := getOption("config", "")
 	if len(configPath) == 0 {
 		offerHelp()
 		os.Exit(1)
